990261b07ffc05584de45a5f0b683959aa9b9896
nvdinh185 <nvdinh0766777123@gmail.com>
2023-05-16 19:56:17 +0700

    review

--- main.js
+++ main.js
@@ -1,3 +1,36 @@
+const listHoa = [
+    {
+        id: 1,
+        tenHoa: 'Hoa Phong Lan',
+        loaiHoa: 'Khai trương',
+        hinhAnh: 'images/hoa1.jpg'
+    },
+    {
+        id: 2,
+        tenHoa: 'Hoa tỷ muội',
+        loaiHoa: 'Khai trương',
+        hinhAnh: 'images/hoa2.jpg'
+    },
+    {
+        id: 3,
+        tenHoa: 'Hoa Violet',
+        loaiHoa: 'Hoa kỷ niệm',
+        hinhAnh: 'images/hoa3.jpg'
+    },
+    {
+        id: 4,
+        tenHoa: 'Hoa thủy tiên',
+        loaiHoa: 'Hoa tình yêu',
+        hinhAnh: 'images/hoa4.jpg'
+    },
+    {
+        id: 5,
+        tenHoa: 'Hoa cẩm chướng',
+        loaiHoa: 'Hoa hạnh phúc',
+        hinhAnh: 'images/hoa5.jpg'
+    }
+]
+
 const tbElement = document.querySelector('#tbl');
 
 // Tiêu đề
@@ -8,48 +41,13 @@ const htmlsTitle = `
             <th>Tên hoa</th>
             <th>Loại hoa</th>
             <th>Hình ảnh</th>
-            <th>Chức năng</th>
         </tr>
     </thead>
     `;
 
 tbElement.innerHTML = htmlsTitle;
 
-const listHoa = [
-    {
-        id: 1,
-        tenHoa: 'Hoa Phong Lan',
-        loaiHoa: 'Khai trương',
-        hinhAnh: 'images/tmp/hoa1.jpg'
-    },
-    {
-        id: 2,
-        tenHoa: 'Hoa tỷ muội',
-        loaiHoa: 'Khai trương',
-        hinhAnh: 'images/tmp/hoa2.jpg'
-    },
-    {
-        id: 3,
-        tenHoa: 'Hoa Violet',
-        loaiHoa: 'Hoa kỷ niệm',
-        hinhAnh: 'images/tmp/hoa3.jpg'
-    },
-    {
-        id: 4,
-        tenHoa: 'Hoa thủy tiên',
-        loaiHoa: 'Hoa tình yêu',
-        hinhAnh: 'images/tmp/hoa4.jpg'
-    },
-    {
-        id: 5,
-        tenHoa: 'Hoa cẩm chướng',
-        loaiHoa: 'Hoa hạnh phúc',
-        hinhAnh: 'images/tmp/hoa5.jpg'
-    }
-]
-
 // Nội dung
-
 var htmlsContents = '';
 for (const hoa of listHoa) {
     htmlsContents += `
@@ -59,10 +57,6 @@ for (const hoa of listHoa) {
         <td>${hoa.loaiHoa}</td>
         <td>
             <img src = '${hoa.hinhAnh}' alt = '${hoa.hinhAnh}' />
-        </td>
-        <td>
-            <a href = '#' title = 'Sửa'><img src = 'images/pencil.gif' alt = 'images/pencil.gif'>Sửa</a>
-            <a href = '#' title = 'Xóa'><img src = 'images/bin.gif' alt = 'images/bin.gif'>Xóa</a>
         </td>
     </tr>`;
 }
