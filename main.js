46c1c4d0b37ef0f55f4ed1d3de8a9742205bd76e
nvdinh185 <nvdinh0766777123@gmail.com>
2023-08-23 15:14:16 +0700

    x

---
+++ main.js
@@ -0,0 +1,32 @@
+const listHoa = [
+    {
+        id: 1,
+        tenHoa: 'Hoa Phong Lan',
+        loaiHoa: 'Khai trương',
+        hinhAnh: 'images/hoa1.jpg'
+    },
+    {
+        id: 2,
+        tenHoa: 'Hoa tỷ muội',
+        loaiHoa: 'Khai trương',
+        hinhAnh: 'images/hoa2.jpg'
+    },
+    {
+        id: 3,
+        tenHoa: 'Hoa Violet',
+        loaiHoa: 'Hoa kỷ niệm',
+        hinhAnh: 'images/hoa3.jpg'
+    },
+    {
+        id: 4,
+        tenHoa: 'Hoa thủy tiên',
+        loaiHoa: 'Hoa tình yêu',
+        hinhAnh: 'images/hoa4.jpg'
+    },
+    {
+        id: 5,
+        tenHoa: 'Hoa cẩm chướng',
+        loaiHoa: 'Hoa hạnh phúc',
+        hinhAnh: 'images/hoa5.jpg'
+    }
+]
